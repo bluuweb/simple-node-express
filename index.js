6dc0fc730e68c9191f78d0fdba43bd90a0e0c8a0
bluuweb <contacto@bluuweb.cl>
2024-04-04 16:51:00 -0300

    fin clase 1

--- index.js
+++ index.js
@@ -6,7 +6,27 @@ app.get('/', (req, res) => {
     res.send('Respondiendo a la ruta raíz 🎉')
 })
 
-// inventar tres rutas más
+app.get('/hello', (req, res) => {
+    res.send('Hola mundo con express 🥳')
+})
+
+// Tarea: Juego adivinar: enviar parámetro y comparar con número aleatorio entre 1 y 3:
+app.get('/game/:number', (req, res) => {
+
+    const numberParams = req.params.number
+
+    res.send(`El número es: ${numberParams}`)
+})
+
+// objeto response
+app.get('/pc', (req, res) => {
+    res.redirect('https://www.pcfactory.cl/producto/50859-samsung-smartphone-galaxy-s23-fe-128gb-8gb-graphite-liberado')
+})
+
+// 404
+app.get('/*', (req, res) => {
+    res.send('404 😣')
+})
 
 // Levantar servidor
 const PORT = process.env.PORT || 5000;
