d628d3b00fee754de708df1155a442829836e3e6
bluuweb <contacto@bluuweb.cl>
2024-04-05 13:33:37 -0300

    users

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 import express from 'express';
 import { authMiddleware } from './middlewares/auth.middleware.js'
 
-const products = [
-    { id: 1, name: "product1", price: 100 },
-    { id: 2, name: "product2", price: 200 },
-    { id: 3, name: "product3", price: 300 },
+const users = [
+    { id: 1, name: "user1", price: 100 },
+    { id: 2, name: "user2", price: 200 },
+    { id: 3, name: "user3", price: 300 },
 ]
 
 const __dirname = import.meta.dirname;
@@ -16,16 +16,16 @@ app.use(express.static(__dirname + '/public'));
 
 // middleware
 app.get('/api/v1/user/profile', authMiddleware, (req, res) => {
-    return res.status(200).send("tu contraseña es: 123123")
+    // quiero devolver el primer usuario
+    const user = users[0]
+    return res.status(200).json(user)
 })
 
-app.get('/api/v1/products', (req, res) => {
-
+app.get('/api/v1/users', (req, res) => {
     // return res.json(products)
-
     return res.json({
         ok: true,
-        products
+        users
     })
 })
 
